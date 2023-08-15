c07d5a789b06ac24583a59bb7435ec22cdaae04a
hartsfield <johnathanhartsfield@gmail.com>
2023-08-15 18:38:07 -0400

    updates

--- viewdata.go
+++ viewdata.go
@@ -5,17 +5,21 @@ import "time"
 // post is the structure of a user post. Posts are created by users and stored
 // in redis.
 type post struct {
-	Title      string    `json:"title" redis:"title"`
-	Id         string    `json:"id" redis:"id"`
-	Author     string    `json:"author,name" redis:"author"`
-	TS         time.Time `json:"ts" redis:"ts"`
-	FTS        string    `json:"fts" redis:"fts"`
-	BodyText   string    `json:"bodytext" redis:"bodytext"`
-	Nonce      string    `json:"nonce" redis:"nonce"`
-	Children   []*post   `json:"children" redis:"children"`
-	ChildCount int       `json:"childCount" redis:"childCount"`
-	Parent     string    `json:"parent" redis:"parent"`
-	PostCount  string    `json:"postCount" redis:"postCount"`
+	Title  string `json:"title" redis:"title"`
+	Id     string `json:"id" redis:"id"`
+	Author string `json:"author,name" redis:"author"`
+	// timestamp
+	TS time.Time `json:"ts" redis:"ts"`
+	// formatted time stamp
+	FTS      string `json:"fts" redis:"fts"`
+	BodyText string `json:"bodytext" redis:"bodytext"`
+	// TODO: implment nonce
+	Nonce      string  `json:"nonce" redis:"nonce"`
+	Children   []*post `json:"children" redis:"children"`
+	ChildCount int     `json:"childCount" redis:"childCount"`
+	Parent     string  `json:"parent" redis:"parent"`
+	// used for pagification
+	PostCount string `json:"postCount" redis:"postCount"`
 }
 
 // viewData represents the root model used to dynamically update the app
