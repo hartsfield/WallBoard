28230039341ef70e33da6abc3f05718ed14632aa
hartsfield <johnathanhartsfield@gmail.com>
2023-08-15 23:49:20 -0400

    updates

--- handlers.go
+++ handlers.go
@@ -46,7 +46,7 @@ func getByChron(w http.ResponseWriter, r *http.Request) {
 
 			var nextCount string
 			// if len(postDBChron) < count {
-			if len(postDBRank) < count && len(postDBRank) < count+20 {
+			if len(postDBRank) < count || len(postDBRank) < count+20 {
 				v.Stream = postDBChron[len(postDBChron)-(count+20-len(postDBChron)):]
 				nextCount = "None"
 			} else {
@@ -95,7 +95,7 @@ func getByRanked(w http.ResponseWriter, r *http.Request) {
 			}
 
 			var nextCount string
-			if len(postDBRank) < count && len(postDBRank) < count+count {
+			if len(postDBRank) < count || len(postDBRank) < count+count {
 				// if len(postDBChron) < count {
 				// v.Stream = postDBRank[len(postDBRank)-(count-len(postDBRank)):]
 				v.Stream = postDBChron[len(postDBChron)-(count+20-len(postDBChron)):]
