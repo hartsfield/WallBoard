388a33bae97405f52d830e1ed168a40021ea1473
John Hartsfield <john@unilink.local>
2024-12-24 23:58:44 -0500

    updates

--- globals.go
+++ globals.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"html/template"
+	"log"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -10,16 +12,46 @@ import (
 
 // ckey/ctxkey is used as the key for the HTML context and is how we retrieve
 // token information and pass it around to handlers
-type ckey int
+// type ckey int
 
-const ctxkey ckey = iota
+// const ctxkey ckey = iota
+
+type env map[string]string
+
+type config struct {
+	App    app    `json:"app"`
+	GCloud gcloud `json:"gcloud"`
+}
+
+type app struct {
+	Name       string `json:"name"`
+	DomainName string `json:"domain_name"`
+	Version    string `json:"version"`
+	Env        env    `json:"env"`
+	Port       string `json:"port"`
+	AlertsOn   bool   `json:"alertsOn"`
+	TLSEnabled bool   `json:"tls_enabled"`
+	Repo       string `json:"repo"`
+}
+
+type gcloud struct {
+	Command   string `json:"command"`
+	Zone      string `json:"zone"`
+	Project   string `json:"project"`
+	User      string `json:"user"`
+	LiveDir   string `json:"livedir"`
+	ProxyConf string `json:"proxyConf"`
+}
 
 var (
 	// servicePort is the port this program will run on
-	servicePort                    = ":" + os.Getenv("servicePort")
-	logFilePath                    = os.Getenv("logFilePath")
-	templates   *template.Template = template.New("main")
-	companyName string             = "BoltApp"
+
+	appConf     *config            = readConf()
+	servicePort                    = ":" + appConf.App.Port
+	logFilePath                    = appConf.App.Env["logFilePath"]
+	templates   *template.Template = template.New("")
+	AppName     string             = appConf.App.Name
+	companyName string             = appConf.App.Name
 	// connect to redis
 	redisIP = os.Getenv("redisIP")
 	rdb     = redis.NewClient(&redis.Options{
@@ -34,3 +66,16 @@ var (
 	postDBChron []*post
 	postDBRank  []*post
 )
+
+func readConf() *config {
+	b, err := os.ReadFile("./bolt.conf.json")
+	if err != nil {
+		log.Println(err)
+	}
+	c := config{}
+	err = json.Unmarshal(b, &c)
+	if err != nil {
+		log.Println(err)
+	}
+	return &c
+}
